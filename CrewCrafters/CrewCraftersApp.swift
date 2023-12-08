3f5aacf5f1ec1c4842c74b1e2f1886b190c7365a
Ishan Joshi <90335897+ishan04joshi@users.noreply.github.com>
2023-12-08 21:57:15 +0530

    project started

---
+++ CrewCrafters/CrewCraftersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewCraftersApp.swift
+//  CrewCrafters
+//
+//  Created by user1 on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CrewCraftersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
