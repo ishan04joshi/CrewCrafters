5f4961eb99ce90b7c693b2f355a6d005688afd3a
Manvi Singhal <manvi.singhal03@gmail.com>
2023-12-15 02:53:54 +0530

    updated profile

--- CrewCrafters/ Views/Profile.swift
+++ CrewCrafters/ Views/Profile.swift
@@ -21,7 +21,7 @@ struct Profile: View {
                         
                         Image("user")
                             .resizable()
-                            .frame(width: 400, height: 250)
+                            .frame(width: 350, height: 250)
                             .aspectRatio(contentMode: .fit)
                             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                             .scaleEffect(x: 0.70, y: 0.70)
@@ -29,10 +29,9 @@ struct Profile: View {
                     }
                     .padding(.bottom, 80.0)
                     
-                    
                     Text("Elon Musk")
                         .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
-                        .bold()
+                        .fontWeight(.bold)
                     Text("Elon Musk")
                         .font(.subheadline)
                         .padding(.bottom)
@@ -45,7 +44,7 @@ struct Profile: View {
                                 .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
                             Text("Projects")
                                 .font(.headline)
-                                
+                            
                         }
                         Spacer()
                         VStack{
@@ -68,10 +67,7 @@ struct Profile: View {
                     
                     
                     Text("Featured")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading)
+                        .profileTitleStyle()
                     
                     ScrollView (.horizontal) {
                         HStack (spacing: 15) {
@@ -89,29 +85,117 @@ struct Profile: View {
                             }
                         }
                     }
-                    .padding(.horizontal)
+                    .padding([.leading, .bottom, .trailing])
                     
+                    Spacer()
                     
                     Text("Tech Stack")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading)
+                        .profileTitleStyle()
                     
                     ScrollView (.horizontal) {
                         HStack (spacing: 10) {
                             ForEach(0..<10){_ in
                                 Text("Sample")
-                                    .padding(8)
+                                    .padding(10)
                                     .background(Color.gray.opacity(0.3))
-                                    .cornerRadius(10)
+                                    .cornerRadius(15)
                                     .lineLimit(1)
                                 
                             }
                         }
                     }
-                    .padding(.horizontal)
+                    .padding([.leading, .bottom, .trailing])
                     
+                    Text("Achievements")
+                        .profileTitleStyle()
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing: 15) {
+                            ForEach(0..<5){_ in
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .strokeBorder(Color.gray.opacity(0.5), lineWidth: 1)
+                                        .frame(width: 150, height: 150)
+                                        .overlay(
+                                            Image("badge")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .padding(10)
+                                        )
+                                }
+                                
+                            }
+                        }
+                    }
+                    .padding([.leading, .bottom, .trailing])
+                    
+                    Text("About")
+                        .profileTitleStyle()
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum enim a felis faucibus ultrices. Donec sed hendrerit justo. Nunc convallis metus eget sem euismod mollis. Praesent laoreet lacus sed mauris viverra porta sit amet non urna.")
+                        .font(.body)
+                        .lineLimit(5)
+                        .padding([.leading, .bottom, .trailing])
+                    
+                    
+                    Text("Hackathons")
+                        .profileTitleStyle()
+                    
+                    ScrollView {
+                        VStack (alignment: .leading, spacing: 15) {
+                            ForEach(0..<3){_ in
+                                HStack(alignment: .top){
+                                    Image("hackathon_poster")
+                                        .resizable()
+                                        .frame(width: 150, height: 90)
+                                        .aspectRatio(contentMode: .fit)
+                                        .cornerRadius(15)
+                                        .padding(.trailing, 5.0)
+                                    VStack(alignment: .leading){
+                                        Text("Appel E Defis")
+                                            .font(.body)
+                                            .fontWeight(.medium)
+                                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum enim a felis")
+                                            .font(.body)
+                                            .foregroundColor(Color.gray)
+                                            .lineLimit(3)
+                                    }
+                                }
+                                Divider()
+                            }
+                        }
+                    }
+                    .padding([.leading, .bottom, .trailing])
+                    
+                    
+                    Text("Endorsements")
+                        .profileTitleStyle()
+                    
+                    ScrollView {
+                        VStack (alignment: .leading, spacing: 10) {
+                            ForEach(0..<3){_ in
+                                HStack{
+                                    Image("user")
+                                        .resizable()
+                                        .frame(width: 50, height: 50)
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                        .padding(.trailing, 5.0)
+                                    VStack(alignment: .leading){
+                                        Text("Bhupinder Jogi")
+                                            .font(.body)
+                                            .fontWeight(.medium)
+                                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing")
+                                            .font(.body)
+                                            .foregroundColor(Color.gray)
+                                            .lineLimit(1)
+                                    }
+                                }
+                                Divider()
+                            }
+                        }
+                    }
+                    .padding([.leading, .bottom, .trailing])
                     
                 }
             }.navigationBarTitleDisplayMode(.inline)
@@ -130,12 +214,26 @@ struct Profile: View {
                     }
                     
                 }.navigationTitle("Profile")
-                
         }
-        
     }
 }
 
+
+struct profileTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title3)
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding([.leading, .bottom])
+    }
+}
+
+extension View {
+    func profileTitleStyle() -> some View {
+        self.modifier(profileTitle())
+    }
+}
 
 struct CustomShape: Shape {
     func path(in rect: CGRect) -> Path {
