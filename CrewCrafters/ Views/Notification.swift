e2741a648567d7a2a13650764162ac130b855e05
Ishan Joshi <90335897+ishan04joshi@users.noreply.github.com>
2024-01-02 22:38:48 +0530

    Notification screen converted to dummy login page
    Temporary update

--- CrewCrafters/ Views/Notification.swift
+++ CrewCrafters/ Views/Notification.swift
@@ -8,10 +8,78 @@
 import SwiftUI
 
 struct Notification: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isPasswordVisible: Bool = false
     var body: some View {
-        Text("Notification View")
+        VStack {
+            Text("Log In")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            
+            TextField("Email Address", text: $email)
+                .textFieldStyle(CustomTextFieldStyle())
+                .padding(.bottom)
+            
+            ZStack(alignment: .trailingFirstTextBaseline) {
+                if isPasswordVisible {
+                    TextField("Password", text: $password)
+                        .textFieldStyle(CustomTextFieldStyle())
+                } else {
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(CustomTextFieldStyle())
+                }
+                
+                Button(action: {
+                    isPasswordVisible.toggle()
+                    if isPasswordVisible {
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
+                            isPasswordVisible = false
+                        }
+                    }
+                }) {
+                    Image(systemName: "eye")
+                        .padding(.trailing, 10)
+                        .foregroundColor(Color.gray)
+                }
+                .padding(.trailing, 20)
+            }
+            
+            HStack(spacing: 0) {
+                Button(action: {
+                    
+                }) {
+                    Text("Forgot Password?")
+                        .font(.subheadline)
+                        .padding(.horizontal)
+                        .foregroundColor(.blue)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+            }
+            .padding(.bottom)
+            
+            Button(
+                action: {
+                    print("Loged In")
+                }, label: {
+                    Text("Sign In")
+                })
+            .buttonStyle(NavigationButton())
+            .navigationBarHidden(true)
+            
+            
+            HStack(spacing: 0) {
+                Text("New around here? ")
+                Text("Sign in")
+                    .foregroundColor(Color.blue)
+            }
+        }
+        .padding()
     }
 }
+
 
 #Preview {
     Notification()
