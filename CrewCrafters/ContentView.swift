5400169260a4664aed9495a1934337fd23845416
arnavkshah15 <100327793+arnavkshah15@users.noreply.github.com>
2024-02-04 15:40:16 +0530

    added firebase

--- CrewCrafters/ContentView.swift
+++ CrewCrafters/ContentView.swift
@@ -6,8 +6,19 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 struct ContentView: View {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some View {
         NavigationView {
             Onboarding()
