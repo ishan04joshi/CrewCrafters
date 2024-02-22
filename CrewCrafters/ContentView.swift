f0e0ca849f26e424d68a546f0e2afebb42396974
Ishan Joshi <90335897+ishan04joshi@users.noreply.github.com>
2024-02-22 14:00:28 +0530

    team database added

--- CrewCrafters/ContentView.swift
+++ CrewCrafters/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            Onboarding()
+            Team_create()
         }
         .navigationBarHidden(true)
     }
---
+++ CrewCrafters/Participant/ViewModels/TeamsViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  TeamsViewModel.swift
+//  CrewCrafters
+//
+//  Created by user1 on 22/02/24.
+//
+
+import Firebase
+import Foundation
+import SwiftUI
+
+
+class TeamsViewModel: ObservableObject {
+    @Published var teams: [Teams] = []
+    
+    @Published var currentTeam: Teams = Teams(
+        name: "",
+        theme: "",
+        problem: "",
+        member_count: 0,
+        tech_stack: []
+    )
+    
+    func addNewTeam(_ teams: Teams) {
+        let db = Firestore.firestore()
+        do {
+          try db.collection("teams").addDocument(data: [
+            "name": teams.name,
+            "theme": teams.theme,
+            "problem": teams.problem,
+            "member_count": teams.tech_stack.count,
+            "techstack": teams.tech_stack
+          ])
+          print("Document successfully written!")
+        } catch {
+          print("Error writing document: \(error)")
+        }
+    }
+}
+
